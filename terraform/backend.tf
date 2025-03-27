78bb571804a7c7c47bdce8601ec8deed217dc57c
Giacomo Sirri <sirrigiacomo9@gmail.com>
2025-03-27 14:19:02 +0100

    Test backend object storage

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,10 +3,9 @@ terraform {
   # as Terraform backend is not suitable when working in a team with a risk of 
   # simultaneous access to the same plan.
   backend "s3" {
-    bucket                      = "terraform-state"
-    key                         = "my_state.tfstate"
-    region                      = "fr-par"
-    endpoint                    = "https://s3.fr-par.scw.cloud"
+    bucket                      = "terraform-state-file-bucket-fr"
+    key                         = "test_project.tfstate"
+    endpoint                    = "https://terraform-state-file-bucket-fr.s3.fr-par.scw.cloud"
     skip_credentials_validation = true
     skip_region_validation      = true
     skip_requesting_account_id  = true
