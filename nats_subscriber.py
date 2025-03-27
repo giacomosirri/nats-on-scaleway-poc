a2860a5c76025903522f1ba5929abf8e4c89ab18
Giacomo Sirri <sirrigiacomo9@gmail.com>
2025-03-27 12:14:02 +0100

    Initialize Terraform module

--- nats_subscriber.py
+++ nats_subscriber.py
@@ -8,7 +8,7 @@ async def message_read(msg):
 async def subscribe():
     nc = await nats.connect("nats://nats.mnq.fr-par.scaleway.com:4222", user_credentials="client1-creds.creds")
     
-    sub = await nc.subscribe("vehicle.speed", cb=message_read)
+    sub = await nc.subscribe("vehicle.*.speed", cb=message_read)
     await asyncio.sleep(60)  # Remain alive for one minute
     await nc.close()
 
---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+provider "scaleway" {
+  project_id      = "d43489e8-6103-4cc8-823b-7235300e81ec"
+  organization_id = "daf36079-e52c-416c-9535-d06742e48acc"
+  region          = "fr-par"
+  zone  	        = "fr-par-1"
+}
+
+variable "project_name" {
+  type = string
+  description = "You can set meaningful names for Scaleway resources by setting this variable."
+  default = "test-project"
+}
+
+resource "scaleway_mnq_nats_account" "main" {
+  name = "nats"-var.project_name
+}
