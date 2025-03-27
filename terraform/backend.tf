5faee3b627a917ac93f7676778ee8e19c08bb11b
Giacomo Sirri <sirrigiacomo9@gmail.com>
2025-03-27 15:50:53 +0100

    Test

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,14 +1,13 @@
-# terraform {
-#   # No locking mechanism is yet supported. Therefore, using Scaleway object storage
-#   # as Terraform backend is not suitable when working in a team with a risk of 
-#   # simultaneous access to the same plan.
-#   backend "s3" {
-#     bucket                      = "terraform-state-file-bucket-fr"
-#     key                         = "test_project.tfstate"
-#     endpoint                    = "https://terraform-state-file-bucket-fr.s3.fr-par.scw.cloud"
-#     region                      = "fr-par"
-#     skip_credentials_validation = true
-#     skip_region_validation      = true
-#     skip_requesting_account_id  = true
-#   }
-# }
+terraform {
+  # No locking mechanism is yet supported. Therefore, using Scaleway object storage
+  # as Terraform backend is not suitable when working in a team with a risk of 
+  # simultaneous access to the same plan.
+  backend "s3" {
+    bucket                      = "terraform-state-file-bucket-fr"
+    key                         = "test_project.tfstate"
+    endpoint                    = "https://terraform-state-file-bucket-fr.s3.fr-par.scw.cloud"
+    region                      = "fr-par"
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+  }
+}
