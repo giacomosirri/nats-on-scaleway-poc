1d7995800c7fc1032c2f162cb1e58e8d6d0da3ce
Giacomo Sirri <sirrigiacomo9@gmail.com>
2025-03-28 16:25:56 +0100

    Continue terraform implementation

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,11 +6,35 @@ provider "scaleway" {
 }
 
 variable "project_name" {
-  type = string
+  type        = string
   description = "You can set meaningful names for Scaleway resources by setting this variable."
-  default = "test-project"
+  default     = "test-project"
 }
 
 resource "scaleway_mnq_nats_account" "main" {
-  name = "nats-${var.project_name}"
+  name        = "nats-${var.project_name}"
+}
+
+resource "scaleway_instance_ip" "public_ip" {}
+
+resource "scaleway_instance_server" "web" {
+  name        = "vm-${var.project_name}"
+  type        = "COPARM1-2C-8G"
+  image       = "ubuntu_noble"
+  ip_id       = scaleway_instance_ip.public_ip.id
+}
+
+resource "scaleway_rdb_instance" "main" {
+  name           = "db-${var.project_name}"
+  node_type      = "DB-DEV-S"
+  engine         = "PostgreSQL-15"
+  is_ha_cluster  = true
+  disable_backup = true
+  user_name      = "my_initial_user"
+  password       = "thiZ_is_v&ry_s3cret"
+}
+
+resource "scaleway_rdb_database" "main" {
+  instance_id    = scaleway_rdb_instance.main.id
+  name           = "test-db-instance"
 }
