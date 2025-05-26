32730ba5a48a8b7a0cc761075240b1843e00f56e
Giacomo Sirri <sirrigiacomo9@gmail.com>
2025-05-26 17:02:17 +0200

    Add provider declaration to child module

--- terraform/kubernetes-module/main.tf
+++ terraform/kubernetes-module/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    scaleway = {
+      source = "scaleway/scaleway"
+    }
+  }
+}
+
 variable "cluster_name" {
   type        = string
   description = "The name of the Kubernetes Kapsule cluster."
