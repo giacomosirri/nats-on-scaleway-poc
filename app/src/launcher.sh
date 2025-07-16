64a41fe81136b66ca51795d899032e453f339619
Giacomo Sirri <sirrigiacomo9@gmail.com>
2025-07-16 10:41:36 +0200

    Add cleanup in launcher.sh

--- app/src/launcher.sh
+++ app/src/launcher.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-python3 ./src/subscribers/nats_aggregator.py &
-python3 ./src/subscribers/nats_subscriber.py &
-python3 ./src/subscribers/nats_subscriber.py &
-./bin/nats_client 123 "fuel" 3
+cleanup() {
+    echo "Cleaning up..."
+    for pid in "${pids[@]}"; do
+        echo "Killing $pid"
+        kill "$pid" 2>/dev/null
+    done
+    exit
+}
+
+pids=()
+
+trap cleanup EXIT
+
+python3 ./src/subscribers/nats_aggregator.py & pids+=($!)
+python3 ./src/subscribers/nats_subscriber.py & pids+=($!)
+python3 ./src/subscribers/nats_subscriber.py & pids+=($!)
+./bin/nats_client 123 "fuel" 3 & pids+=($!)
+
+wait
