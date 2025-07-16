c36cee8b24c674f69eebd784351af085fe23a209
Giacomo Sirri <sirrigiacomo9@gmail.com>
2025-07-16 14:31:48 +0200

    Improve logic and logs quality in data consumer

--- app/src/subscribers/nats_subscriber.py
+++ app/src/subscribers/nats_subscriber.py
@@ -3,25 +3,31 @@ import nats
 import sys
 import os
 from functools import partial
-    
+from nats_credentials_handler import get_current_localized_time, write_nats_credentials_to_file, get_nats_credentials
+
+valid_keys = [
+    "location"
+    "fuel"
+    "speed"
+    "brake_temp"
+]
+
 async def message_read(kv, msg):
-    print(f"Message received on subject: {msg.subject}")
     topic = msg.subject.split('.')[2]
-    if topic not in data.keys():
-        print(f"Message is on an unexpected subject: {msg.subject}")
+    if topic not in valid_keys:
+        print(f"[WARNING][{get_current_localized_time()}] Data consumer received a message on an unexpected subject: {msg.subject}. Skipping...")
     else:
         value = float(msg.data.decode())
-        print(f"{topic}: {value}")
-        data[topic].append((float(msg.data.decode())))
+        print(f"[WARNING][{get_current_localized_time()}] New value received by data consumer. Subject: {msg.subject}, value: {value}. Saving this data in the KeyValue store...")
         await kv.put(msg.subject, str(value).encode())
 
-async def subscribe():
+async def subscribe(nats_credentials_file):
     # Connect to the NATS server.
-    nc = await nats.connect("nats://nats.mnq.fr-par.scaleway.com:4222", user_credentials=FILE_PATH)
+    nc = await nats.connect("nats://nats.mnq.fr-par.scaleway.com:4222", user_credentials=nats_credentials_file)
     if not nc.is_connected:
-        print("Subscriber failed to connect to NATS server.")
+        print(f"[ERROR][{get_current_localized_time()}] Data consumer failed to connect to NATS server.")
         return False
-    print("Subscriber connected to NATS server.")
+    print(f"[INFO][{get_current_localized_time()}] Subscriber connected to NATS server.")
 
     # Create JetStream context.
     js = nc.jetstream()
@@ -30,37 +36,20 @@ async def subscribe():
     kv = await js.key_value('telemetry')
 
     # Subscribe to the topic.
-    subject = await nc.subscribe(f"vehicle.*.*", queue="queue1", cb=partial(message_read, kv))
-    print(f"Subscribed to topic: vehicle.*.*. Waiting for messages...")
+    topic = "vehicle.*.*"
+    subject = await nc.subscribe(topic, queue="queue1", cb=partial(message_read, kv))
+    print(f"[INFO][{get_current_localized_time()}] Data consumer subscribed to topic: {topic}.")
 
     # Keep the subscriber running to listen for messages.
     try:
         await asyncio.Event().wait()
     except (KeyboardInterrupt, asyncio.CancelledError):
-        print("Shutting down...")
-        entries: list = await kv.history("vehicle.123.fuel")
-        for entry in entries:
-            print(f"Key: {entry}")
+        print(f"[INFO][{get_current_localized_time()}] Data consumer received a shutdown signal. Shutting down...")
         # Remove interest in subscription.
         await subject.unsubscribe()
         # Terminate connection to NATS.
         await nc.drain()
-        return True
 
-data = {
-    "location": [],
-    "fuel": [],
-    "speed": [],
-    "brake_temp": []
-}
-
-FILE_PATH = "/".join([os.getcwd(), "secrets/nats-credentials.txt"])
-
-res = asyncio.run(subscribe())
-if not res:
-    print("Something went wrong during the subscription process. Shutting down...")
-    sys.exit(1)
-else:
-    print("Subscriber has been shut down successfully.")
-    print(f"Data received: {data}", flush=True)
-    sys.exit(0)
+if __name__ == "__main__":
+    credentials_file_path = write_nats_credentials_to_file(get_nats_credentials())
+    res = asyncio.run(subscribe(credentials_file_path))
